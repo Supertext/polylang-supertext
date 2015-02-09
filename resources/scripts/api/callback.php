93f387500ecd97b66e77a373c6d4382a6bca66b1
Michael <michael@comotive.ch>
2015-02-09 08:25:45 +0100

    change loader

--- resources/scripts/api/callback.php
+++ resources/scripts/api/callback.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once $_SERVER['DOCUMENT_ROOT'] . '/wp-admin/admin.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/wp-load.php';
 
 use Supertext\Polylang\Api\Wrapper;
 use Supertext\Polylang\Api\Multilang;
