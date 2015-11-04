8c14f96f4b6b0ca2b45139577aadf4570a81e28a
Heinrich Muralt <h_muralt@bluewin.ch>
2015-11-04 12:26:01 +0100

    added more clearer commment to ICustomFieldProvider

--- src/Supertext/Polylang/Helper/ICustomFieldProvider.php
+++ src/Supertext/Polylang/Helper/ICustomFieldProvider.php
@@ -1,12 +1,6 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: heinrich
- * Date: 14.10.15
- * Time: 09:31
- */
-namespace Supertext\Polylang\Helper;
 
+namespace Supertext\Polylang\Helper;
 
 /**
  * The ACF Custom Field provider
@@ -14,10 +8,22 @@ namespace Supertext\Polylang\Helper;
  */
 interface ICustomFieldProvider
 {
+  /**
+   * @return string the plugin name
+   */
   public function getPluginName();
 
   /**
-   * @return array multidimensional list of custom fields defintions
+   * Gets the Custom Field Definitions as multidimensional array.
+   *
+   * @return array multidimensional list of custom fields defintions, keys:
+   * <pre>
+   *     id: (string) an id to identify custom field definition
+   *     label: (string) a label
+   *     type: (string) 'group' or 'field'
+   *     meta_key_regex: (string) regex to match custom field key (usually db meta_key is sufficient)
+   *     sub_field_definitions: (array) child field definitions
+   * </pre>
    */
   public function getCustomFieldDefinitions();
 }
