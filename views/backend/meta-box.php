f39e621cefbcda131241c7452917eafd7d058a9c
Heinrich Muralt <h_muralt@bluewin.ch>
2017-01-23 17:15:00 +0100

    Hide modified info if post is in translation

--- views/backend/meta-box.php
+++ views/backend/meta-box.php
@@ -7,7 +7,7 @@
         $status['isInTranslation'] ?
           _e('This post is currently being modified by Supertext.', 'polylang-supertext') :
           _e('This post is a translation.', 'polylang-supertext'); ?>
-      <?php $status['hasChangedSinceLastTranslation'] ? _e("Its content has been modified and doesn't match the original translation anymore.", 'polylang-supertext') : ''; ?>
+      <?php $status['hasChangedSinceLastTranslation'] && !$status['isInTranslation'] ? _e("Its content has been modified and doesn't match the original translation anymore.", 'polylang-supertext') : ''; ?>
     </p>
     <p><button type="button" class="button" <?php echo $status['hasChangedSinceLastTranslation'] && !$status['isInTranslation'] ? '' : 'disabled="disabled"'; ?> onclick="Supertext.Polylang.sendSyncRequest()"><?php _e('Send changes to Supertext', 'polylang-supertext'); ?></button></p>
 <?php
