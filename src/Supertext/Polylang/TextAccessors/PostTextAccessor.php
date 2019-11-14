951314ca47b418588797f77d452af04ec7377eed
Heinrich Muralt <heinrich@supertext.ch>
2019-11-14 10:36:05 +0100

    Extended PostTextAccessor to handle block attriubtes (limited to string values only)

--- src/Supertext/Polylang/TextAccessors/PostTextAccessor.php
+++ src/Supertext/Polylang/TextAccessors/PostTextAccessor.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Supertext\Polylang\TextAccessors;
+
 use Supertext\Polylang\Helper\TextProcessor;
 
 /**
@@ -27,7 +28,8 @@ class PostTextAccessor implements ITextAccessor
     $this->textProcessor = $textProcessor;
   }
 
-  public function uncheckPostContentPerDefault(){
+  public function uncheckPostContentPerDefault()
+  {
     $this->isPostContentCheckedPerDefault = false;
   }
 
@@ -95,6 +97,10 @@ class PostTextAccessor implements ITextAccessor
       $texts['post_content'] = $this->textProcessor->replaceShortcodes($post->post_content);
     }
 
+    if ($selectedTranslatableFields['post_content'] && use_block_editor_for_post($post)) {
+      $texts['post_content_block_attributes'] = $this->getTranslatableBlockAttributes($post);
+    }
+
     if ($selectedTranslatableFields['post_excerpt']) {
       $texts['post_excerpt'] = $post->post_excerpt;
     }
@@ -109,6 +115,11 @@ class PostTextAccessor implements ITextAccessor
   public function setTexts($post, $texts)
   {
     foreach ($texts as $id => $text) {
+      if ($id === 'post_content_block_attributes') {
+        $post->post_content = $this->setTranslatableBlockAttributes($text, $post->post_content);
+        continue;
+      }
+
       $decodedContent = html_entity_decode($text, ENT_COMPAT | ENT_HTML401, 'UTF-8');
 
       if ($id === 'post_content') {
@@ -116,6 +127,91 @@ class PostTextAccessor implements ITextAccessor
       }
 
       $post->{$id} = $decodedContent;
+    }
+  }
+
+  private function getTranslatableBlockAttributes($post)
+  {
+    $translatableBlockAttributes = array();
+    $blocks = parse_blocks($post->post_content);
+
+    foreach ($blocks as $index => $block) {
+      if (empty($block['attrs'])) {
+        continue;
+      }
+
+      $translatableBlockAttributesTexts = $this->getTranslatableBlockAttributesTexts($block['attrs']);
+
+      if (empty($translatableBlockAttributesTexts)) {
+        continue;
+      }
+
+      $translatableBlockAttributes[$index] = $translatableBlockAttributesTexts;
+    }
+
+    return $translatableBlockAttributes;
+  }
+
+  private function getTranslatableBlockAttributesTexts($attributes)
+  {
+    $translatableBlockAttributesTexts = array();
+
+    foreach ($attributes as $key => $value) {
+      if (!is_string($value)) {
+        continue;
+      }
+
+      $translatableBlockAttributesTexts[$key] = $value;
+    }
+
+    return $translatableBlockAttributesTexts;
+  }
+
+  private function setTranslatableBlockAttributes($translatableBlockAttributes, $content)
+  {
+    $newContent = '';
+    $blocks = parse_blocks($content);
+
+    foreach ($blocks as $index => $block) {
+      if (isset($translatableBlockAttributes[$index])) {
+        foreach ($translatableBlockAttributes[$index] as $key => $value) {
+          $block['attrs'][$key] = $value;
+        }
+      }
+
+      $newContent .= $this->serializeBlock($block);
+    }
+
+    return $newContent;
+  }
+
+  private function serializeBlock($block)
+  {
+    if (!isset($block['blockName'])) {
+      return false;
+    }
+    $name = $block['blockName'];
+    if (0 === strpos($name, 'core/')) {
+      $name = substr($name, strlen('core/'));
+    }
+    if (empty($block['attrs'])) {
+      $opening_tag_suffix = '';
+    } else {
+      $opening_tag_suffix = ' ' . json_encode($block['attrs']);
+    }
+    if (empty($block['innerHTML'])) {
+      return sprintf(
+        '<!-- wp:%s%s /-->',
+        $name,
+        $opening_tag_suffix
+      );
+    } else {
+      return sprintf(
+        '<!-- wp:%1$s%2$s -->%3$s<!-- /wp:%1$s -->',
+        $name,
+        $opening_tag_suffix,
+        $block['innerHTML']
+      );
     }
   }
 }
