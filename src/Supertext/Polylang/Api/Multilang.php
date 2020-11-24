b29e9f725574228bd3148534aa76e37d25483f73
Heinrich Muralt <heinrich@supertext.ch>
2020-11-24 08:42:41 +0100

    Updated AdminExtension to use new Multilang API

--- src/Supertext/Polylang/Api/Multilang.php
+++ src/Supertext/Polylang/Api/Multilang.php
@@ -9,29 +9,16 @@ namespace Supertext\Polylang\Api;
  */
 class Multilang
 {
+
+  private static $multilangApi = null;
+
   /**
    * Get all in polylang configured languages
    * @return \PLL_Language[] list of languages
    */
   public static function getLanguages()
   {
-    if (function_exists('pll_languages_list')) {
-      return pll_languages_list(
-        array('fields' => array())
-      );
-    }
-
-    if(self::is_wpml()){
-      $languageList = icl_get_languages();
-      $langObjs = array();
-      foreach($languageList as $lang){
-        $langObj = array('name' => $lang['native_name'], 'slug' => $lang['code']);
-        array_push($langObjs, (object) $langObj);
-      }
-     return $langObjs;
-    }
-
-    return array();
+    return self::getMultilangApi()->getLanguages();
   }
 
   /**
@@ -40,22 +27,7 @@ class Multilang
    */
   public static function getPostLanguage($postId)
   {
-    if (function_exists('pll_get_post_language')) {
-      return pll_get_post_language($postId);
-    }
-
-    if(self::is_wpml()){
-      $postLang = apply_filters(
-        'wpml_element_language_code',
-        null,
-        array( 'element_id'=> $postId , 'element_type'=> 'post')
-      );
-      if($postLang !== null){
-        return $postLang;
-      }
-    }
-
-    return false;
+    return self::getMultilangApi()->getPostLanguage($postId);
   }
 
   /**
@@ -64,24 +36,7 @@ class Multilang
    */
   public static function getTermLanguage($termId)
   {
-    if (function_exists('pll_get_term_language')) {
-      return pll_get_term_language($termId);
-    }
-
-    if(self::is_wpml()){
-      $termTax = get_term($termId)->taxonomy;
-      $termLang = apply_filters(
-        'wpml_element_language_code',
-        null,
-        array( 'element_id'=> $termId , 'element_type'=> $termTax)
-      );
-
-      if($termLang !== null){
-        return $termLang;
-      }
-    }
-
-    return false;
+    return self::getMultilangApi()->getTermLanguage($termId);
   }
 
   /**
@@ -91,16 +46,7 @@ class Multilang
    */
   public static function getPostInLanguage($postId, $language)
   {
-    if (function_exists('pll_get_post')) {
-      return pll_get_post($postId, $language);
-    }
-
-    if(self::is_wpml()){
-      $postType = get_post_type($postId);
-      return icl_object_id($postId, $postType, false, $language);
-    }
-
-    return null;
+    return self::getMultilangApi()->getPostInLanguage($postId, $language);
   }
 
   /**
@@ -110,16 +56,16 @@ class Multilang
    */
   public static function getTermInLanguage($termId, $language)
   {
-    if (function_exists('pll_get_term')) {
-      return pll_get_term($termId, $language);
-    }
+    return self::getMultilangApi()->getTermInLanguage($termId, $language);
+  }
 
-    if(self::is_wpml()){
-      $termTax = get_term($termId)->taxonomy;
-      return icl_object_id($termId, $termTax, false, $language);
-    }
-
-    return null;
+  /**
+   * Get the urls for creating a new post for the different languages
+   * @return array two dimensional array with [post id][language code] to url mapping
+   */
+  public static function getNewPostUrls()
+  {
+    return self::getMultilangApi()->getNewPostUrls();
   }
 
   /**
@@ -127,135 +73,53 @@ class Multilang
    * @param $language
    * @param $trid
    */
-  public static function setPostLanguage($postId, $language, $trid = false){
-    if (function_exists('pll_set_post_language')) {
-      pll_set_post_language($postId, $language);
-    }
-
-    if(self::is_wpml()){
-      $postType = get_post_type($postId);
-      do_action( 'wpml_set_element_language_details', array(
-        'element_id' => $postId,
-        'element_type' => 'post_' . $postType,
-        'trid' => $trid,
-        'language_code' => $language
-      ));
-    }
-    // for args explanation visit: https://wpml.org/wpml-hook/wpml_set_element_language_details/
-
+  public static function setPostLanguage($postId, $language, $trid = false)
+  {
+    self::getMultilangApi()->setPostLanguage($postId, $language, $trid);
   }
 
   /**
    * @param $termId
    * @param $language
    */
-  public static function setTermLanguage($termId, $language){
-    if (function_exists('pll_set_term_language')) {
-      pll_set_term_language($termId, $language);
-    }
-
-    if(self::is_wpml()){
-      $termType = get_term_by('id', $termId)->taxonomy;
-      do_action( 'wpml_set_element_language_details', array(
-        'element_id' => $termId,
-        'element_type' => 'tax_' . $termType,
-        'trid' => false,
-        'language_code' => $language
-      ));
-      // for args explanation visit: https://wpml.org/wpml-hook/wpml_set_element_language_details/
-    }
+  public static function setTermLanguage($termId, $language)
+  {
+    self::getMultilangApi()->setTermLanguage($termId, $language);
   }
 
   /**
    * @param array $arr an associative array of translations with language code as key and post id as value
    */
-  public static function savePostTranslations($arr){
-    if (function_exists('pll_save_post_translations')) {
-      pll_save_post_translations($arr);
-    }
-
-    if(self::is_wpml()){
-      foreach($arr as $langCode => $postId){
-        $postTrid = apply_filters('wpml_element_trid', null, $postId);
-        $postType = get_post_type($postId);
-
-        $postSrcLang = apply_filters('wpml_element_language_details',  null, array('element_id' => $postId, 'element_type' => $postType));
-          global $wpdb;
-          $wpdb->update(
-            $wpdb->prefix.'icl_translations',
-            array(
-              'trid' => $postTrid,
-              'language_code' => $langCode,
-              'source_language_code' => $postSrcLang
-            ),
-            array(
-              'element_type' => $postType,
-              'element_id' => $postId
-            )
-          );
-      }
-    }
-    // source: https://wordpress.stackexchange.com/questions/20143/plugin-wpml-how-to-create-a-translation-of-a-post-using-the-wpml-api
-    // source: https://wordpress.stackexchange.com/questions/147652/how-to-update-records-using-wpdb
+  public static function savePostTranslations($arr)
+  {
+    self::getMultilangApi()->savePostTranslations($arr);
   }
 
   /**
    * @param array $arr an associative array of translations with language code as key and term id as value
    */
-  public static function saveTermTranslations($arr){
-    if (function_exists('pll_save_term_translations')) {
-      pll_save_term_translations($arr);
-    }
-
-    if(self::is_wpml()){
-      foreach($arr as $langCode => $termId){
-        $termTrid = apply_filters('wpml_element_trid', null, $termId);
-        $tridSrcLang = apply_filters('wpml_element_language_details',  null, array('element_id' => $termId, 'element_type' => 'tax_category'))->source_language_code;
-        global $wpdb;
-        $wpdb->update(
-          $wpdb->prefix.'icl_translations',
-          array(
-            'trid' => $termTrid,
-            'language_code' => $langCode,
-            'source_language_code' => $tridSrcLang
-          ),
-          array(
-            'element_type' => 'tax_category',
-            'element_id' => $termId
-          )
-        );
-      }
-    }
+  public static function saveTermTranslations($arr)
+  {
+    self::getMultilangApi()->saveTermTranslations($arr);
   }
 
   /**
-   * Check if the CMS has wmpl and if it is active
-   * @return bool returns true if wpml is installed and active
+   * Get whether WPML is active or not
    */
-  public static function is_wpml(){
+  public static function isWpmlActive()
+  {
     return (function_exists('icl_object_id') && is_plugin_active('sitepress-multilingual-cms/sitepress.php'));
   }
 
   /**
-   * @param $trid translation-post id (same as $original_id)
-   * @param $lang_code language code
-   * @param $source_language source language code
-   * @param $original_id original/source post
-   * @return string absolute link to create a translation for a post
+   * @return \Supertext\Polylang\Api\IMultilangApi
    */
-  public static function generate_wpml_link( $trid, $lang_code, $source_language, $original_id )
+  private static function getMultilangApi()
   {
-    $link = 'post-new.php?' . http_build_query(
-        array(
-          'lang' => $lang_code,
-          'post_type' => get_post_type($original_id),
-          'trid' => $trid,
-          'source_lang' => $source_language,
-          'from_post' => $original_id,
-          'new_lang' => $lang_code
-        )
-      );
+    if (self::$multilangApi == null) {
+      self::$multilangApi = self::isWpmlActive() ? new WPMLApiWrapper() : new PolylangApiWrapper();
+    }
 
-    return admin_url() . $link;
+    return self::$multilangApi;
   }
-} 
+}
--- src/Supertext/Polylang/Backend/AdminExtension.php
+++ src/Supertext/Polylang/Backend/AdminExtension.php
@@ -59,11 +59,6 @@ class AdminExtension
   private $isCurrentPostInTranslation = false;
 
   /**
-   * @var array
-   */
-  private $newPostUrls = Array();
-
-  /**
    * Various filters to change and/or display things
    */
   public function __construct($library, $log)
@@ -88,11 +83,10 @@ class AdminExtension
     add_action('manage_pages_custom_column', array($this, 'displayTranslationStatusColumn'), 12, 2);
     add_action('manage_media_custom_column', array($this, 'displayTranslationStatusColumn'), 12, 2);
 
-    add_filter('pll_get_new_post_translation_link', array($this, 'addNewPostUrl'), 100, 3);
-
-    if(isset($_GET[Constant::NEW_POST_AUTO_SAVE_FLAG]) && isset($_GET['from_post']) && isset($_GET['new_lang'])){
+    if(isset($_GET[Constant::NEW_POST_AUTO_SAVE_FLAG]) && isset($_GET['source_post'])){
+      
       add_filter('wp_insert_post_data', array($this, 'setNewTargetPostData'), 1000, 1);
-      add_action('save_post', array($this, 'assignLanguageToNewTargetPost'), 10, 1);
+      add_action('save_post', array('Supertext\Polylang\Api\Multilang', 'assignLanguageToNewTargetPost'), 10, 1);
     }
   }
 
@@ -204,32 +198,7 @@ class AdminExtension
     }
 
     $pluginStatus = $this->library->getPluginStatus();
-
-    // replacement for Polylang filter 'pll_get_new_post_translation_link' -> adds posts url to javascript context
-    if(Multilang::is_wpml()) {
-
-      $allLanguages = Multilang::getLanguages();
-      global $posts;
-
-      if (count((array)$posts) > 0) {
-        foreach ($posts as $thePost) {
-
-          $postId = $thePost->ID;
-          foreach ($allLanguages as $language) {
-            $newPostLink = Multilang::generate_wpml_link($postId, $language->slug, Multilang::getPostLanguage($postId), $postId);
-            $this->addNewPostUrl($newPostLink, $language, $postId);
-          }
-        }
-      } else {
-        global $post;
-        $postId = $post->ID;
-
-        foreach ($allLanguages as $language) {
-          $newPostLink = Multilang::generate_wpml_link($postId, $language->slug, Multilang::getPostLanguage($postId), $postId);
-          $this->addNewPostUrl($newPostLink, $language, $postId);
-        }
-      }
-    }
+    $newPostUrls = Multilang::getNewPostUrls();
 
     $context = array(
       'enable' => $pluginStatus->isPolylangActivated &&
@@ -241,7 +210,7 @@ class AdminExtension
       'isCurrentPostInTranslation' => $this->isCurrentPostInTranslation,
       'resourceUrl' => get_bloginfo('wpurl'),
       'ajaxUrl' => admin_url( 'admin-ajax.php' ),
-      'newPostUrls' => $this->newPostUrls,
+      'newPostUrls' => $newPostUrls,
       'newPostAutoSaveFlag' => Constant::NEW_POST_AUTO_SAVE_FLAG
     );
 
@@ -303,17 +272,6 @@ class AdminExtension
     ));
   }
 
-  public function addNewPostUrl($link, $language, $post_id)
-  {
-    if(!isset($this->newPostUrls[$post_id])){
-      $this->newPostUrls[$post_id] = Array();
-    }
-
-    $this->newPostUrls[$post_id][$language->slug] = urldecode(html_entity_decode($link));
-
-    return $link;
-  }
-
   /**
    * Sets the translation status column cell
    * @param $column
@@ -359,8 +317,8 @@ class AdminExtension
    * @param $data post data as array
    */
   public function setNewTargetPostData($data){
-    $sourcePost = get_post($_GET['from_post']);
-
+    $sourcePost = get_post($_GET['source_post']);
+    
     $data['post_status'] = Constant::TRANSLATION_POST_STATUS;
     $data['post_title'] = $sourcePost->post_title . ' [' . __('In translation', 'polylang-supertext') . '...]';
     
