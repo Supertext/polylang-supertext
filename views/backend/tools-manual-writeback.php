d0339fd7728a4979984eab9463d0047c11f0e483
Heinrich Muralt <heinrich@supertext.ch>
2021-03-02 11:35:51 +0100

    Fixed text domain issues

--- views/backend/tools-manual-writeback.php
+++ views/backend/tools-manual-writeback.php
@@ -3,7 +3,7 @@
 ?>
 <div class="postbox postbox_admin">
   <div class="inside">
-    <h3><?php _e('Manual writeback', 'polylang-supertext'); ?></h3>
+    <h3><?php _e('Manual writeback', 'supertext'); ?></h3>
     <textarea name="writebackData" width="100%"></textarea>
   </div>
 </div>
